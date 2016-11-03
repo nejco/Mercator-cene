cea527e29250537287a2f599551a02668362bf0d
Nejc Vidrih <nejc.vidrih@gmail.com>
2016-11-03 13:39:31 +0100

    Initial Commit

---
+++ Mercator cene/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mercator cene
+//
+//  Created by Nejc Vidrih on 03/11/2016.
+//  Copyright © 2016 Nejc Vidrih. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
